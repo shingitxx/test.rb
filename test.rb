2f207a734e40944cb8c55ad6c50c68247e8ed98f
shingitxx <nextstagexx@gmail.com>
2020-07-17 08:47:15 +0900

    1回目

---
+++ test.rb
@@ -0,0 +1,13 @@
+def price_with_shipping(price)
+	# priceが5000以上のとき、戻り値としてpriceを返すif文を作成してください
+	
+
+  # priceに500を加えた値を戻り値として返してください
+	
+end
+
+puts "商品の合計金額は3000円です"
+puts "お支払い金額は、送料込みで#{price_with_shipping(3000)}円です"
+puts "-----------"
+puts "商品の合計金額は10000円です"
+puts "お支払い金額は、送料込みで#{price_with_shipping(10000)}円です"
